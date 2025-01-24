538e1cd8eb8d3a0a4aa5b3fd3ded0590d259d0c8
tehw0lf <tehwolf@protonmail.com>
2025-01-24 12:20:17 +0100

    Remove obsolete var

--- env.example.py
+++ env.example.py
@@ -2,6 +2,4 @@ import os
 
 base_path = f"{os.path.expanduser('~')}/Downloads"
 base_url = "https://mystreamingsite.com"
-chromedriver_path = f"{os.path.expanduser('~')}/.local/bin/chromedriver"
-
 favorites = ["nyancat"]
