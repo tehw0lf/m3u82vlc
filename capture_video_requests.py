a7ee3a3ed7ce90e2765a1f21ecb0610040b3a958
tehw0lf <tehwolf@protonmail.com>
2025-09-28 22:10:57 +0200

    Support getting URL from request and response

--- capture_video_requests.py
+++ capture_video_requests.py
@@ -1,10 +1,17 @@
+import json
+
 from mitmproxy import http
+
 import env
 
 
 def request(flow: http.HTTPFlow) -> None:
     if ".m3u8" in flow.request.pretty_url:
         print(flow.request.pretty_url)
+    if flow.response and flow.response.content:
+        json_data = json.loads(flow.response.content).get("url")
+        if json_data and json_data.endswith(".m3u8"):
+            print(json_data)
     if env.log_file:
         with open(env.log_file, "a") as log:
             log.write(flow.request.pretty_url + "\n")
