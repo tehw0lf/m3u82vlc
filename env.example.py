e3d69650c09ad6684a6f82a5cb7042e3b4b5e1dd
tehw0lf <tehwolf@protonmail.com>
2025-09-29 14:10:29 +0200

    Remove base_url from example

--- env.example.py
+++ env.example.py
@@ -1,5 +1,4 @@
 import os
 
 base_path = f"{os.path.expanduser('~')}/Downloads"
-base_url = "https://mystreamingsite.com"
-favorites = ["nyancat"]
+favorites = ["https://mystreamingsite.com/nyancat"]
